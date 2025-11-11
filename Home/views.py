365b612cdad5717a0214baad5c9deb4eb759415b
Bilal Raza <bil321al@gmail.com>
2025-11-11 10:02:32 +0500

    Update the project

--- Home/views.py
+++ Home/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is homepage")
+    context = {
+        "variable0": "This is sent from views.py",
+        "variable1": "Greetings, welcome to our website!",
+    }
+    return render(request, "index.html", context)
+    # return HttpResponse("This is homepage")
+
 def about(request):
     return HttpResponse("This is about page")
 def services(request):

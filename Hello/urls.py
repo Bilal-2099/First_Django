a858d80cc4b3cebc9626a1ad3936d79ae3cf75bf
Bilal Raza <bil321al@gmail.com>
2025-11-14 23:51:46 +0500

    Daily Practice

--- Hello/urls.py
+++ Hello/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Hello Admin"
+admin.site.site_title = "Hello Admin Portal"
+admin.site.index_title = "Welcome to Hello Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Home.urls'))

15292f482f0e97ad47984a14cb224fede71a3f86
Bilal Raza <bil321al@gmail.com>
2025-11-16 22:54:17 +0500

    Daily Django Practice

--- Home/views.py
+++ Home/views.py
@@ -10,8 +10,11 @@ def index(request):
     # return HttpResponse("This is homepage")
 
 def about(request):
-    return HttpResponse("This is about page")
+    # return HttpResponse("This is about page")
+    return render(request, "about.html")
 def services(request):
-    return HttpResponse("This is services page")
+    # return HttpResponse("This is services page")
+    return render(request, "services.html")
 def contact(request):
-    return HttpResponse("This is contact page")
+    # return HttpResponse("This is contact page")
+    return render(request, "contact.html")

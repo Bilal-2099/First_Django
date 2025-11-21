a8c476b345cbcfdecb370488d9c3490b88a7a987
Bilal Raza <bil321al@gmail.com>
2025-11-21 23:58:47 +0500

    Something?

--- Home/views.py
+++ Home/views.py
@@ -3,14 +3,10 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "index.html")
-    # return HttpResponse("This is homepage")
-
+    
 def about(request):
-    # return HttpResponse("This is about page")
     return render(request, "about.html")
 def services(request):
-    # return HttpResponse("This is services page")
     return render(request, "services.html")
 def contact(request):
-    # return HttpResponse("This is contact page")
-    return render(request, "contact.html")
+     return render(request, "contact.html")

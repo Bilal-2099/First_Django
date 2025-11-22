1087674f156b35dee32287e786ba46f1baf0a947
Bilal Raza <bil321al@gmail.com>
2025-11-22 11:37:34 +0500

    Erroe

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-
+from datetime import datetime
+from .models import Contact
 # Create your views here.
 def index(request):
     return render(request, "index.html")
@@ -9,4 +10,10 @@ def about(request):
 def services(request):
     return render(request, "services.html")
 def contact(request):
+     if request.method == "POST":
+         name = request.POST.get('name')
+         email = request.POST.get('email')
+         password = request.POST.get('password')
+         contact = Contact(name=name, email=email, password=password,  date=datetime.today())
+         contact.save()
      return render(request, "contact.html")

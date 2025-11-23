8a4f3b577e59fe267fbae102c91b6255e7b642f1
Bilal Raza <bil321al@gmail.com>
2025-11-23 13:03:01 +0500

    fixing Error

--- Home/models.py
+++ Home/models.py
@@ -6,3 +6,6 @@ class Contact(models.Model):
     email = models.CharField(max_length=122)
     password = models.CharField(max_length=12)
     date = models.DateField()
+
+def __str__(self):
+    return self.name
